0bce685b289e900ad90f9f878b7a623c2d6d11f6
Keith Davis <kpdavis.kd@outlook.com>
2017-03-03 15:19:36 -0700

    ready for test

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alsdfjl;kasdjflkjasd;lfjlkasdjflkasv
+$(document).ready(function() {
+
+    var url = 'https://api.punkapi.com/v2/beers',
+        code = '',
+        items = [];
+
+
+    $.get(url).then(function(data) {
+        for (var input in data) {
+            code +=  addBeers(food_pairing[input], data)
+        }
+      console.log(code);
+    })
+})
+
+function addBeers(input, data) {
+  var bool = input.text().includes($('#search').text())
+  if (bool == true) {
+    return data.name[food]
+  }
+}
