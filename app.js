c4dcf2fd7833b545e726a9cc8d6b6f7aba20b311
Derek Kramer <derek.kramer88@gmail.com>
2017-03-03 11:19:49 -0700

    Alphabetized

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-var url = 'https://api.punkapi.com/v2/beers';
-
-var code = '';
+var url = 'https://api.punkapi.com/v2/beers',
+    code = '',
+    items = [];
 
 $.get(url).then(function(data){
     code += '<select>';
     for (var input in data) {
-            for (var food in data[input].food_pairing) {
-                // console.log(data[input].food_pairing[food]);
-
-                code += '<option value="option">' + data[input].food_pairing[food] + '</option>';
-            }
+        for (var food in data[input].food_pairing) {
+            items.push(data[input].food_pairing[food]);
         }
+    }
+
+    items = items.sort();
+
+    for (var i = 0; i < items.length; i++) {
+        code += '<option value="option">' + items[i] + '</option>';
+    }
+
     code += '</select>';
 
     console.log(code);
